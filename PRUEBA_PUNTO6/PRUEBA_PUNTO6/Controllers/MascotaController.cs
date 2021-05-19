4935a39a6be580a6dfc3dc53de7ca10de10161b7
Juan Diego Arboleda G <juandiegoguepe@gmail.com>
2021-05-18 19:49:33 -0500

    Importando repositorio, API ok

---
+++ PRUEBA_PUNTO6/PRUEBA_PUNTO6/Controllers/MascotaController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using Newtonsoft.Json;
+namespace PRUEBA.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MascotaController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            IEnumerable<modelo.mascota> lst = new List<modelo.mascota>();
+            using (var db = new MySqlConnection("Server=localhost; Database=pruebasiesa; Uid=root;"))
+            {
+                var sql = "select * from mascota";
+                lst = db.Query<modelo.mascota>(sql).ToList();
+            }
+            string jsonString = JsonConvert.SerializeObject(lst);
+            return Ok(jsonString);
+        }
+    }
+}
---
+++ PRUEBA_PUNTO6/PRUEBA_PUNTO6/modelo/mascota.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PRUEBA.modelo
+{
+    public class mascota
+    {
+        public int id_mascota { get; set; }
+        public string nombre { get; set; }
+        public int id_tipomascota { get; set; }
+        public int id_propietario { get; set; }
+        public string adoptada { get; set; }
+
+    }
+}
